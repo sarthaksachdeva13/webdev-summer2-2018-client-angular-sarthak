5c9aa7fa128ec23bc09b1b2c6e37c46550469920
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-04 20:24:17 -0400

    Added course and user models and course viewer

--- webdev-angular/src/app/course-viewer/course-viewer.component.ts
+++ webdev-angular/src/app/course-viewer/course-viewer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {CourseServiceClient} from '../services/course.service.client';
+import {Course} from '../models/course.model.client';
 
 @Component({
   selector: 'app-course-viewer',
@@ -7,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CourseViewerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: CourseServiceClient,
+              private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.loadCourse(params['courseId']));
+  }
+
+  course: Course = new Course();
+  loadCourse(courseId) {
+    this.service.findCourseById(courseId)
+      .then(course => this.course = course);
+  }
 
   ngOnInit() {
   }
--- webdev-angular/src/app/services/course.service.client.ts
+++ webdev-angular/src/app/services/course.service.client.ts
@@ -2,8 +2,33 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class CourseServiceClient {
+
+  COURSE_URL = 'http://localhost:8080/api/course';
+  AUTH_URL = 'http://localhost:4000/api/user/auth';
+
   findAllCourses() {
     return fetch('http://localhost:8080/api/course')
       .then(response => response.json());
+  }
+
+  findCourseById(courseId) {
+    return fetch('http://localhost:8080/api/course' + '/' + courseId)
+      .then(response => response.json());
+  }
+
+  authenticate() {
+    return fetch(this.AUTH_URL, {
+      credentials: 'include'
+    }).then(response => (response.json()));
+  }
+
+  findEnrolledCoursesForStudent(courseIds) {
+    return fetch('http://localhost:8080/api/courses', {
+      method: 'POST',
+      body: JSON.stringify(courseIds),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then((response) => (response.json()));
   }
 }
