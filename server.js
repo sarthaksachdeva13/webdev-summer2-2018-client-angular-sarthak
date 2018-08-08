9f1db68aae7cb83a4f614fd31ce04239d308dedd
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-07 21:57:15 -0400

    server.js edits

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/webdev-summer2-2018-client-angular-sarthak'));
+app.use(express.static('/dist/webdev-summer2-2018-client-angular-sarthak'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/webdev-summer2-2018-client-angular-sarthak/index.html'));
+  res.sendFile(path.join('/dist/webdev-summer2-2018-client-angular-sarthak/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
