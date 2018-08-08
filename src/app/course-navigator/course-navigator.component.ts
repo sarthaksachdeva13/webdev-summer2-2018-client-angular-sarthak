0a2b69048c51062881986e93fab75369ed82e4d2
Sarthak Sachdeva <sarthaksachdeva13@gmail.com>
2018-08-07 20:21:16 -0400

    minor edits

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -8,36 +8,19 @@ import {CourseServiceClient} from '../services/course.service.client';
 })
 export class CourseNavigatorComponent implements OnInit {
 
-  courses = [];
-  selectedCourse = {};
-  selectedModule = {};
-  selectedLesson = {};
-  selectedTopic = {};
-
   constructor(private courseService: CourseServiceClient) {
   }
 
-  selectCourse(course) {
-    this.selectedCourse = course;
+  courses = [];
+  modules = [];
+
+  selectCourse(courseId) {
+    this.courseService.findAllModulesForCourses(courseId)
+      .then(modules => this.modules = modules);
   }
-
-  selectModule(module) {
-    this.selectedModule = module;
-  }
-
-  selectLesson(lesson) {
-    this.selectedLesson = lesson;
-  }
-
-
-  selectTopic(topic) {
-    this.selectedTopic = topic;
-  }
-
 
   ngOnInit() {
-    this.courseService
-      .findAllCourses()
+    this.courseService.findAllCourses()
       .then(courses => this.courses = courses);
   }
 
--- src/app/services/course.service.client.ts
+++ src/app/services/course.service.client.ts
@@ -18,6 +18,10 @@ export class CourseServiceClient {
       credentials: 'include'
     }).then(response => (response.json()))
 
+  findAllModulesForCourses = (courseId) =>
+    fetch('http://localhost:8080/api/course/' + courseId + '/module')
+      .then(response => response.json())
+
 
   findEnrolledCoursesForStudent(courseIds) {
     return fetch('http://localhost:8080/api/courses', {
